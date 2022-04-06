439f47d62b520ba8c0248010c9035a4760b68de3
flavio <flavio@172.16.0.2>
2022-04-06 20:51:44 -0300

    projeto Arquivos

---
+++ modulo_datas/src/modulo_datas/DatasEmJava8.java
@@ -0,0 +1,38 @@
+package modulo_datas;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.Period;
+
+
+public class DatasEmJava8 {
+
+	public static void main(String[] args) throws ParseException, InterruptedException {
+		
+		LocalDate dataAntiga = LocalDate.parse("2019-10-05");
+		
+		LocalDate dataNova= LocalDate.parse("2021-10-10");
+		
+		System.out.println("Data antiga é maior que data nova: " + dataAntiga.isAfter(dataNova));
+		
+		
+		System.out.println("Data antiga é anterior a nova: " + dataAntiga.isBefore(dataNova));
+		
+		System.out.println("Datas são iguais : " + dataAntiga.isEqual(dataNova));
+		
+		Period periodo = Period.between(dataAntiga, dataNova);
+		
+		System.out.println("Quantos dias : " + periodo.getDays());
+		
+		
+		System.out.println("Quantos meses : " + periodo.getMonths());
+		
+		System.out.println("Quantos Anos : " + periodo.getYears());
+		
+		System.out.println(" Somente meses: " + periodo.toTotalMonths());
+		
+		System.out.println("Periodo é : " + periodo.getYears() + " anos " + periodo.getMonths() + " meses " + periodo.getDays() + " dias.");
+
+	}
+
+}
